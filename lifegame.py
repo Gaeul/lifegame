7cf38deaa21c315a5e4fe5522509e74397e979ea
JinYeJin <yeen6667@gmail.com>
2015-06-21 23:57:11 +0900

    Complete

--- lifegame.py
+++ lifegame.py
@@ -1,71 +1,113 @@
 import copy
+import curses
+import lifegame
+
 
 def load():
-    inputfile = open("/home/user/Python-3.4.3/lifegame/input_data.txt",'r')
+    inputfile = open("/home/user/git/lifegame/input_data.txt",'r')
     data = inputfile.read()
     inputfile.close()
     return data
  
 
 def set_board(data):
-    print(data)
     game_cell = []
-    for i in range(0, 8):
-        game_cell.append(data[(i*9) : ((i+1)*9 -1) ])
+    for i in range(0, 20):
+        game_cell.append(data[(i*51) : ((i+1)*51 -1) ])
+    
+    cur_board = [[0 for col in range(50)] for row in range(20)]
 
-    cur_board = [[0 for col in range(8)] for row in range(8)]
-
-    for i in range(0, len(game_cell)):
-        for j in range(0, len(game_cell)):
-            if(game_cell[i][j]=='x'):
-                cur_board[i][j]=0
+    for i in range(20):
+        for j in range(50):
+            if(game_cell[i][j]=='X'):
+                cur_board[i][j] = 0
             else:
-                cur_board[i][j]=1
-
+                cur_board[i][j] = 1
     return cur_board
 
 def printBoard(board):
-    stdscr.clear()
+    win1 = curses.newwin(22, 52, 1, 2)
+    curses.noecho()
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.attron(curses.color_pair(1))
     stdscr.border("#","#","#","#","#","#","#","#")
-    stdscr.addstr(cur_board)
-    stdscr.refresh()
+    stdscr.addstr(20,30,"Press any key to START")
     stdscr.getch()
-    board = next_board
+    stdscr.refresh()
+           
     pass
 
 def runLifeGame(board):
-    cur_board = copy.deepcopy(board)
-    next_board = copy.deepcopy(cur_board)
+    cur_board = [[0 for col in range(50)] for row in range(20)]
+    next_board = [[0 for col in range(50)] for row in range(20)]
 
-    alive_cell = 0
+    for i in range(20):
+        for j in range(50):
+            cur_board[i][j] = next_board[i][j] = board[i][j]
+            
+    for a in range(20):
+        for b in range(50):
+            alive_cell = 0
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if i==0 and j==0:
+                        continue
+                    try :
+                        if cur_board[a + i][b + j] == 1:
+                            alive_cell += 1
+                    except IndexError:
+                        continue
+                        
 
-    x = cur_board
-    y = cur_board
+            if cur_board[a][b] == 1 and (alive_cell <= 1 or alive_cell >= 4):
+                next_board[a][b] = 0
+            
+            elif cur_board[a][b] == 1  and (alive_cell == 3 or alive_cell == 2):
+                continue
 
-    for a in range(8):
-        for i in range(0, 2):
-            for j in range(0, 2):
-                if cur_board[i][j] == 1:
-                    alive_cell += 1
-    x += 1
-    y += 1
-
-    return alive_cell
-
-
-    if cur_board ==1 and (alive_cell <= 1 or alive_cell >= 4):
-        next_board[i][j] = 0
-    else:
-        pass
-    
-    if cur_board == 1 and alive_cell == 3:
-        next_board[i][j] = 1
-    else:
-        pass
+            elif cur_board[a][b] == 0 and alive_cell == 3:
+                next_board[a][b] = 1
 
     pass
     return next_board
+
+def newWindow(board):
+
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    win1.attron(curses.color_pair(2))
+    win1.border("#","#","#","#","#","#","#","#")
+    for i in range(20):
+        for j in range(50):
+            y = i+1
+            x = j+1
+            if board[i][j] == 1:
+                win1.addstr(y, x, "*")
+            else:
+                win1.addstr(y, x, " ")
+    win1.refresh()
+
+
+def startGame():
+    cur_board = []
+    next_board = []
+
+    data = load()
+    cur_board = set_board(data)
+    time = 500
+    time_integration = 0
+    #printBoard(cur_board)
+    win1.nodelay(1)
+    while True:
+        c = win1.getch()
+        if c != -1:
+            break
+        next_board = runLifeGame(cur_board)
+        newWindow(next_board)
+        cur_board = next_board
+
+        curses.napms(time)
+        time_integration+=time
+        
+        if(time_integration>5000):
+            break
--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-
 import sys
-sys.path.append("/home/Python-3.4.3/lifegame")
+sys.path.append("/home/user/git/lifegame")
 
-import lifegame, os, curses
-
-data = lifegame.load()
-cur_board = lifegame.set_board(data)
-print(cur_board)
-time = 500
-time_integration = 0
+import lifegame, os
+import curses
 
 lifegame.stdscr = curses.initscr()
 curses.curs_set(0)
-
-lifegame.printBoard(next_board)
-
+lifegame.win1 = curses.newwin(22, 52, 1, 2)
+curses.noecho()
+curses.start_color()
+curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
 while True:
-    next_board = lifegame.runLifeGame(cur_board) 
-    lifegame.printBoard(next_board)
-    cur_board = next_board
-
-    curses.napms(time)
-    time_integration+=time
-    if(time_integration>5000):
-        break
+    lifegame.stdscr.attron(curses.color_pair(1))
+    lifegame.stdscr.border("#","#","#","#","#","#","#","#")
+    lifegame.stdscr.addstr(20,30,"Press any key to START")
+    lifegame.stdscr.getch()
+    lifegame.stdscr.refresh()
+    lifegame.startGame()
     
 curses.endwin()
